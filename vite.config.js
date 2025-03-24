8a5e7ffe37a4f5e24b23e9b2629a784a6751e6b8
Harshal <harshal@Harshals-MacBook-Air.local>
2025-03-24 13:21:56 +0530

    Set up GitHub Actions for deployment1

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/14harshaldhote/harshaldhote_11/', // Updated base path to match the GitHub repo name
   plugins: [react()],
 })
